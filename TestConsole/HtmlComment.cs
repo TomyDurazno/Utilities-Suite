dce96e34cb212d7179b5f443c58e12abb3c6b885
TomyDurazno <tomy.durazno@gmail.com>
2018-10-28 23:44:45 -0300

    Added 'Invoker' classes in 'TestConsole' project

---
+++ TestConsole/HtmlComment.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Text;
+using static Utility.Core.Attributes.InvokerAttributes;
+
+namespace ConsoleTest
+{
+    [Invoker("hcomment")]
+    public class HtmlComment
+    {
+        [InvokerCaller]
+        public string Dum(string[] arguments)
+        {
+            return arguments.Aggregate(new StringBuilder(), (builder, s) => builder.Append(string.Format("<!--{0}--> ", s))).ToString();
+        }
+    }
+}
---
+++ TestConsole/HtmlNested.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Text;
+using static Utility.Core.Attributes.InvokerAttributes;
+
+namespace ConsoleTest
+{
+    [Invoker("hnested")]
+    public class HtmlNested
+    {
+        [InvokerCaller]
+        public string Dum(string[] arguments)
+        {
+            return arguments.Reverse()
+                            .Aggregate(string.Empty, (acum, s) => string.Format(string.IsNullOrEmpty(acum) ? "<{1}></{1}>" : "<{1}>{0}</{1}>", acum, s));            
+        }
+    }
+}
